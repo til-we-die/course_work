7f4b0d513da295670bee88c313ed2db47a4bc448
Starova Ekaterina <starovae6@gmail.com>
2024-11-11 14:14:44 +0300

    Token update added, database changed to MySQL.

--- course_work/accounts/urls.py
+++ course_work/accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import UserListCreate
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('api/users/', UserListCreate.as_view(), name='user-list-create'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
