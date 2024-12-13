1e7f7ccd0402f542a0786a2e4e9b4a079400ee89
Starova Ekaterina <starovae6@gmail.com>
2024-12-13 20:01:34 +0300

    Added error handling during registration.

--- course_work/accounts/serializers.py
+++ course_work/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,3 +17,24 @@ class UserSerializer(serializers.ModelSerializer):
         user = User(**validated_data)
         user.save()
         return user
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Имя пользователя уже используется.")
+        return value
+
+    def validate_email(self, value):
+        try:
+            validate_email(value)
+        except ValidationError:
+            raise serializers.ValidationError("Введите корректный адрес электронной почты.")
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Электронная почта уже используется.")
+        return value
+
+    def validate_password(self, value):
+        if len(value) < 8 or not any(c.isdigit() for c in value) or not any(c.isalpha() for c in value):
+            raise serializers.ValidationError(
+                "Пароль должен содержать минимум 8 символов, включать буквы и цифры."
+            )
+        return value
