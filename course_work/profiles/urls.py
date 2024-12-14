ae65c3f43ccedff2dddfc4a3c95e23de2cac588b
Starova Ekaterina <starovae6@gmail.com>
2024-12-15 00:30:36 +0300

    Update README and urls for profiles

--- course_work/profiles/urls.py
+++ course_work/profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserProfileView, PostListCreateView
 
 urlpatterns = [
-    path('profile/', UserProfileView.as_view(), name='user-profile'),
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('api/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('api/posts/', PostListCreateView.as_view(), name='post-list-create'),
 ]
